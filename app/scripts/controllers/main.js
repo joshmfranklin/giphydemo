e15431b8a59ed99f8dcab83a8030edca8dd61577
Josh Franklin <jfrank7588@gmail.com>
2015-07-18 21:25:57 -0400

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name workspaceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the workspaceApp
+ */
+angular.module('workspaceApp')  
+  .controller('MainCtrl', function ($scope, $http) {
+
+  	var pendingTask;
+
+    if ($scope.search === undefined) {
+      $scope.search = "cute cats";
+      fetch();
+    }
+
+    $scope.change = function() {
+      if (pendingTask) {
+        clearTimeout(pendingTask);
+      }
+      pendingTask = setTimeout(fetch, 800);
+    };
+
+    function fetch() {
+      $http.get("http://api.giphy.com/v1/gifs/search?q=" + $scope.search + "&rating=r&limit=75&api_key=dc6zaTOxFJmzC")
+        .success(function(response) {
+          $scope.search = response;
+        });
+
+    }  	
+
+  });
